88669fb4b6f3861abbad4093a0f444fac36b8c5f
Eric <73317463+NycRat@users.noreply.github.com>
2023-06-25 20:07:52 -0700

    more bored sites

--- frontend/src/bored/BoredPage.tsx
+++ frontend/src/bored/BoredPage.tsx
@@ -1,45 +1,58 @@
-import {
-  closestCenter,
-  DndContext,
-  DragEndEvent,
-  PointerSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
-import {
-  arrayMove,
-  rectSortingStrategy,
-  SortableContext,
-} from "@dnd-kit/sortable";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BoredSite from "./BoredSite";
 
 const BoredPage = () => {
-  const sensors = useSensors(
-    useSensor(PointerSensor, {
-      activationConstraint: {
-        distance: 20,
-      },
-    })
-  );
   const [sites, setSites] = useState([
-    { id: 1, url: "https://nycrat.github.io/tic-tac-toe/" },
-    { id: 2, url: "https://justtypestuff.com/" },
-    { id: 3, url: "https://nycrat.github.io/tic-tac-toe/" },
-    { id: 4, url: "https://justtypestuff.com/" },
+    "https://zoomquilt.org/",
+    "https://justtypestuff.com/",
+    "https://nycrat.github.io/tic-tac-toe/",
+    "https://optical.toys/",
+    "https://checkbox.toys/",
+    "https://paint.toys/",
+    "https://maze.toys/",
+    "https://puginarug.com/",
+    "https://longdogechallenge.com/",
+    "https://weirdorconfusing.com/",
+    "https://mondrianandme.com/",
+    "https://onesquareminesweeper.com/",
+    "https://floatingqrcode.com/",
+    "https://sliding.toys/",
+    "https://alwaysjudgeabookbyitscover.com/",
+    "https://cursoreffects.com/",
+    "https://tholman.com/binary-music-player/",
+    "https://smashthewalls.com/",
+    "http://corndog.io/",
+    "https://heeeeeeeey.com/ ",
+    "http://eelslap.com/",
+    "https://checkboxrace.com/",
+    "http://drawing.garden/",
+    "https://trypap.com/",
+    "http://www.everydayim.com/",
+    "http://randomcolour.com/",
+    "https://cat-bounce.com/",
+    "http://maninthedark.com/",
+    "https://chrismckenzie.com/",
+    "http://www.koalastothemax.com/",
+    "https://www.trashloop.com/",
+    "http://www.rrrgggbbb.com/",
+    "https://pointerpointer.com/",
+    "http://lacquerlacquer.com/",
+    "https://www.yesnoif.com/",
+    "https://thisisnotajumpscare.com/",
+    "http://chihuahuaspin.com/",
+    "http://spaceis.cool/",
   ]);
 
-  const handleDragEnd = ({ active, over }: DragEndEvent) => {
-    if (over && active.id !== over.id) {
-      const activeIndex = sites.findIndex(({ id }) => id === active.id);
-      const overIndex = sites.findIndex(({ id }) => id === over.id);
-
-      setSites(arrayMove(sites, activeIndex, overIndex));
-    }
-  };
-
   const navigate = useNavigate();
+
+  const handleSetActive = (i: number) => {
+    let newSites = [...sites];
+    const temp = newSites[i];
+    newSites[i] = newSites[0];
+    newSites[0] = temp;
+    setSites(newSites);
+  };
 
   return (
     <div className="background-dimmer">
@@ -47,21 +60,24 @@ const BoredPage = () => {
         What should I do with my life?
       </h1>
       <div className="bored-page">
-        <DndContext
-          sensors={sensors}
-          collisionDetection={closestCenter}
-          onDragEnd={handleDragEnd}
-        >
-          <SortableContext items={sites} strategy={rectSortingStrategy}>
-            {sites.map((idea, i) => (
-              <BoredSite
-                key={idea.id}
-                {...idea}
-                active={i === 0}
-              />
-            ))}
-          </SortableContext>
-        </DndContext>
+        <div>
+          <BoredSite url={sites[0]} active={true} setActive={() => {}} />
+        </div>
+        <div className="things">
+          {sites.map(
+            (site, i) =>
+              i !== 0 && (
+                <div>
+                  <BoredSite
+                    key={i}
+                    url={site}
+                    active={i === 0}
+                    setActive={() => handleSetActive(i)}
+                  />
+                </div>
+              )
+          )}
+        </div>
       </div>
     </div>
   );
--- frontend/src/bored/BoredSite.tsx
+++ frontend/src/bored/BoredSite.tsx
@@ -1,41 +1,13 @@
-import { UniqueIdentifier } from "@dnd-kit/core";
-import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
-import { CSSProperties } from "react";
-
 interface BoredSiteProps {
-  id: UniqueIdentifier;
   url: string;
   active: boolean;
+  setActive: () => void;
 }
 
 const BoredSite = (props: BoredSiteProps) => {
-  const {
-    attributes,
-    isDragging,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-  } = useSortable({ id: props.id });
-
-  const style: CSSProperties = {
-    opacity: isDragging ? 0.5 : undefined,
-    transform: CSS.Translate.toString(transform),
-    zIndex: isDragging ? 99 : 0,
-    transition,
-  };
-
   return (
-    <div
-      className="bored-site"
-      /* onBlur={() => setActive(false)} */
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-    >
-      {!props.active && <div>Drag To Focus</div>}
+    <div className="bored-site" onClick={() => props.setActive()}>
+      {!props.active && <div />}
       <iframe src={props.url} title="description" />
     </div>
   );
