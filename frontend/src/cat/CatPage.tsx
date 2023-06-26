8d76227495bc87e6ceec710d0cfaf617da35b8f8
Eric <73317463+NycRat@users.noreply.github.com>
2023-06-25 19:09:44 -0700

    cat.

--- frontend/src/cat/CatPage.tsx
+++ frontend/src/cat/CatPage.tsx
@@ -8,6 +8,10 @@ const CatPage = () => {
       <h1 className="header-title" onClick={() => navigate("/")}>
         What should I do with my life?
       </h1>
+      <video className="cat-video" autoPlay loop>
+        <source src="/what-should-i-do-with-my-life/cat.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       cat
     </div>
   );
